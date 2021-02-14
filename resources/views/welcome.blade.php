26459d0c619ffac9f98201280a5ac747ffab2f5e
praztyobudi <praztyob@gmail.com>
2021-02-14 15:27:46 +0700

    coba2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Latihan
                 </div>
 
                 <div class="links">
