d29950f72d59c08a082a23c238c429f1604c0dc9
praztyobudi <50093557+praztyobudi@users.noreply.github.com>
2021-02-14 15:29:51 +0700

    hapus edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Latihan
+                    Laravel
                 </div>
 
                 <div class="links">
